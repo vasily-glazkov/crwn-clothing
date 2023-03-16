16a10234e1220a50a3eb806d24a1ca6adc2ac83f
vasily-glazkov <vglazkoff@gmail.com>
2023-03-17 00:06:12 +0300

    changed navbar styling, addded logo

--- src/routes/navigation/Navigation.jsx
+++ src/routes/navigation/Navigation.jsx
@@ -1,20 +1,28 @@
-import React from 'react';
-import {Link, Outlet} from "react-router-dom";
+import {Fragment} from 'react';
+import {Outlet, Link} from 'react-router-dom';
+
+import {ReactComponent as CrwnLogo} from '../../assets/crown.svg';
+
+import './navigation.styles.scss';
 
 const Navigation = () => {
-    return (
-        <>
-            <div className='navigation'>
-                <Link className='logo-container' to='/'>
-                    <div>Logo</div>
-                </Link>
-                <div className="nav-links-container">
-                    <Link className='nav-link' to='/shop'>SHOP</Link>
-                </div>
-                <Outlet/>
-            </div>
-        </>
-    );
+	
+	return (
+		<Fragment>
+			<div className='navigation'>
+				<Link className='logo-container' to='/'>
+					<CrwnLogo className='logo'/>
+				</Link>
+				<div className='nav-links-container'>
+					<Link className='nav-link' to='/shop'>
+						SHOP
+					</Link>
+				
+				</div>
+			</div>
+			<Outlet/>
+		</Fragment>
+	);
 };
 
 export default Navigation;
