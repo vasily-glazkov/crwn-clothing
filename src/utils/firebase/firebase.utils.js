a428fa20d90742b1a830c712628b0707d90e50ca
vasily-glazkov <vglazkoff@gmail.com>
2023-03-18 19:54:56 +0300

    created firebase db and createUserDocumentFromAuth

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,6 +5,12 @@ import {
 	signInWithRedirect,
 	GoogleAuthProvider
 } from 'firebase/auth';
+import {
+	getFirestore,
+	doc,
+	getDoc,
+	setDoc
+} from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -26,6 +32,16 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+	const userDocRef = doc(db, 'users', userAuth.uid)
+	
+	console.log(userDocRef);
+	const userSnapshot = await getDoc(userDocRef);
+	console.log(userSnapshot);
+	console.log(userSnapshot.exists());
+}
 
 
 
