8ff1514689fd5891500c0593e492d2e1beb8c20d
vasily-glazkov <vglazkoff@gmail.com>
2023-03-19 15:02:50 +0300

    imported getRdirectResult from firebase/auth

--- src/routes/sign-in/SignIn.jsx
+++ src/routes/sign-in/SignIn.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
 import {
+	auth,
 	signInWithGooglePopup,
 	createUserDocumentFromAuth,
 	signInWithGoogleRedirect
 } from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
+	useEffect(async () => {
+		const response = await getRedirectResult;
+	})
 
 	const logGoogleUser = async () => {
-		const {user} = await signInWithGooglePopup();
+		const { user } = await signInWithGooglePopup();
 		const userDocRef = await createUserDocumentFromAuth(user);
 	}
 
 	const logGoogleRedirectUser = async () => {
-		const {user} = await signInWithGoogleRedirect();
+		const { user } = await signInWithGoogleRedirect();
 		console.log(user);
 	}
 
