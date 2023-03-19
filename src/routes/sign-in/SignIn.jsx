d8e3e614bb94a8ecaf1b761de7e1c06052b35bd9
vasily-glazkov <vglazkoff@gmail.com>
2023-03-19 14:49:13 +0300

    RedirectUser added

--- src/routes/sign-in/SignIn.jsx
+++ src/routes/sign-in/SignIn.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
+import {
+	signInWithGooglePopup,
+	createUserDocumentFromAuth,
+	signInWithGoogleRedirect
+} from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
+
 	const logGoogleUser = async () => {
 		const {user} = await signInWithGooglePopup();
 		const userDocRef = await createUserDocumentFromAuth(user);
 	}
+
+	const logGoogleRedirectUser = async () => {
+		const {user} = await signInWithGoogleRedirect();
+		console.log(user);
+	}
+
+
 	return (
 		<div>
 			<h2>Sign in page</h2>
 			<button onClick={logGoogleUser}>Sign In With Google</button>
+			<button onClick={logGoogleRedirectUser}>Sign In With Google Redirect</button>
 		</div>
 	);
 };
